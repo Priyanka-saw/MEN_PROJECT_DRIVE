03bf10acb6d2a4f45925085f248a85670da79348
Priyanka-saw <priyankasaw51@gmail.com>
2025-08-18 15:32:48 +0530

    initial commit

---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userScheme = new mongoose.Schema({
+    username: {
+
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        minlength: [3, 'username must be at least 3 characters long']
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        minlength: [13, 'email must be at least 13 characters long']
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [5, 'password must be at least 5 characters long']
+    }
+
+
+})
+
+const user = mongoose.model('user', userScheme)
+
+module.exports = user;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
 
 
 // user/test
@@ -8,14 +13,87 @@ router.get('/register', (req, res) => {
     res.render("register")
 })
 
-router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.send('uer register')
+router.post('/register',
+    body('email').trim().isEmail().isLength({ min: 13 }),
+    body('password').trim().isLength({ min: 5 }),
+    body('username').trim().isLength({ min: 3 }),
+
+    async (req, res) => {
+
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'invalid data'
+            })
+        }
+
+        const { email, username, password } = req.body;
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await userModel.create({
+            email,
+            username,
+            password: hashPassword
+        })
+
+        res.json(newUser)
+
+    })
+
+
+
+router.get('/login', (req, res) => {
+    res.render('login');
 })
 
 
+router.post('/login',
+    body('username').trim().isLength({ min: 3 }),
+    body('password').trim().isLength({ min: 5 }),
+    async (req, res) => {
 
 
+        const errors = validationResult(req)
 
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'invalid data'
+            })
+        }
+
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne({
+            username: username
+        })
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+        // jsonwebtoken (with the help of this token pata karate hai ki user login hai ki nahi)
+
+        const token = jwt.sign({
+            userId:user._id,
+            email: user.email
+        })
+
+
+    }
+
+)
 
 module.exports = router
